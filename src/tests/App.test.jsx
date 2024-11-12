79aec25948dbe7e6f0589a4f16334aa5be4f487c
Joe Winter <joewinteruk@gmail.com>
2024-11-12 11:42:25 +0000

    added react library

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,7 +1,17 @@
 import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
 
 describe("An example test", () => {
   it("should be true", () => {
-    expect(true).toBe(true)
-  })
-})
+    expect(true).toBe(true);
+  });
+});
+
+describe("App", () => {
+  it("renders the App component and prints to terminal", () => {
+    render(<App />);
+
+    screen.debug();
+  });
+});
