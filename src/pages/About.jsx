4f033c05ec608658b3afdcb5df28f573b827e0dc
Joe Winter <joewinteruk@gmail.com>
2024-11-12 15:39:34 +0000

    added tests for about and home pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+
+export default function About() {
+  return (
+    <>
+    <h4>About Me</h4>
+    </>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <>
+    <h4>Hi there, I&apos;m Joe Winter</h4>
+    </>
+  )
+}
