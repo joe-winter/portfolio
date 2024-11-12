b5f5a1b9e0af64faa671e54f432d794f44cece88
username <username@example.com>
2024-11-12 18:03:26 +0000

    set up tailwind and added navbar

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,9 @@
+import NavBar from "../Components/NavBar";
 
 export default function About() {
   return (
     <>
+    <NavBar />
     <h4>About Me</h4>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import NavBar from "../Components/NavBar";
 
 export default function Home() {
   return (
     <>
+    <NavBar />
     <h4>Hi there, I&apos;m Joe Winter</h4>
     </>
   )
