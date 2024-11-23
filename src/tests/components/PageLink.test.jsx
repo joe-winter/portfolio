533a868fb69d105ff4d9a19961b8c2b1d8085661
Joe Winter <joewinteruk@gmail.com>
2024-11-23 14:43:56 +0000

    formatted page link tests

--- src/tests/components/PageLink.test.jsx
+++ src/tests/components/PageLink.test.jsx
@@ -3,24 +3,22 @@ import { describe, expect, it, vi } from "vitest";
 import PageLink from "../../Components/PageLink";
 import { useNavigate } from "react-router-dom";
 
-vi.mock('react-router-dom', () => {
-  const navigateMock = vi.fn()
-  const useNavigateMock = () => navigateMock
-  return { useNavigate: useNavigateMock}
+vi.mock("react-router-dom", () => {
+  const navigateMock = vi.fn();
+  const useNavigateMock = () => navigateMock;
+  return { useNavigate: useNavigateMock };
+});
 
-})
+describe("PageLink", () => {
+  it("should call usenaivgate on the given path with clicked", () => {
+    render(<PageLink path={"/test"} text={"Test"} />);
 
+    const buttonEl = screen.getByRole("button");
+    fireEvent.click(buttonEl);
 
-describe('PageLink', () => {
-  it('should call usenaivgate on the given path with clicked', () => {
-    render(<PageLink path={'/test'} text={'Test'}/>)
+    const navigateMock = useNavigate();
+    expect(navigateMock).toHaveBeenCalledWith("/test");
 
-    const buttonEl = screen.getByRole('button')
-    fireEvent.click(buttonEl)
-
-    const navigateMock = useNavigate()
-    expect(navigateMock).toHaveBeenCalledWith('/test')
-
-    expect(buttonEl).toHaveTextContent('Test')
-  })
-})
+    expect(buttonEl).toHaveTextContent("Test");
+  });
+});
