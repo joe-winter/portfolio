155d950009c2ad31e4d9195c71b7cf9df7172ad9
Joe Winter <joewinteruk@gmail.com>
2024-12-03 09:40:39 +0000

    adding next project

--- public/constants/projects.js
+++ public/constants/projects.js
@@ -17,6 +17,25 @@ const projects = [
     ],
     images: "/public/images/MakersUIGame/host-landing.png",
   },
+  {
+    id: 1,
+    title: 'Acebook',
+    description: 
+      "A Facebook clone with React, Node.js, Express and MongoDB, featuring authentication, dynamic posts, profiles, friends, likes and messages",
+    github: "https://github.com/Alexia-May/acebook-watermelon-club",
+    link: "https://example.com/",
+    technologies: [
+      "/public/icons/react.svg",
+      "/public/icons/node.svg",
+      "/public/icons/jwt.svg",
+      "/public/icons/express.svg",
+      "/public/icons/mongodb.svg",
+      "/public/icons/vitest.svg",
+      "/public/icons/vite.svg",
+
+    ],
+    images: "/public/images/Acebook/home-page.png"
+  }
 ];
 
 export default projects;
