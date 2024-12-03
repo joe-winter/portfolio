a73fee0496aa5bef501f40fa18d10d6657823429
Joe Winter <joewinteruk@gmail.com>
2024-12-03 08:42:41 +0000

    changed json to js file

--- public/constants/projects.js
+++ public/constants/projects.js
@@ -5,7 +5,7 @@ const projects = [
     "description": "A multiplayer number guessing game with lobbies, round timers, real-time updates and deployed via Render with CI/CD using GitHub Actions.",
     "github": "https://github.com/shammy642/MakersUIGame",
     "link": "https://example.com/",
-    "technologies": ["/public/icons/react.svg", "/public/icons/github.svg"],
+    "technologies": ["/public/icons/react.svg", "/public/icons/node.svg", "/public/icons/socketio.svg"],
     "image": "/public/images/MakersUIGame/host-landing.png"
   }
 ]
