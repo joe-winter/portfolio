15077edee7b6d26fafcdd4c16967e1887dd1ae17
Joe Winter <joewinteruk@gmail.com>
2024-12-05 12:54:44 +0000

    updated navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
@@ -20,7 +19,12 @@ function App() {
 
   return (
     <ThemeProvider>
-      <NavBarScroll scrollToProjects={() => scrolltoRef(projectsRef)} />
+      <NavBarScroll
+        scrollToProjects={() => scrolltoRef(projectsRef)}
+        scrollToAbout={() => scrolltoRef(projectsRef)}
+        scrollToHome={() => scrolltoRef(projectsRef)}
+        scrollToContact={() => scrolltoRef(projectsRef)}
+      />
       <section ref={homeRef}>
         <Home />
       </section>
@@ -38,38 +42,3 @@ function App() {
 }
 
 export default App;
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: (
-//       <>
-//         <NavBar /> <Home />
-//       </>
-//     ),
-//   },
-//   {
-//     path: "/about",
-//     element: (
-//       <>
-//         <NavBar /> <About />
-//       </>
-//     ),
-//   },
-//   {
-//     path: "/projects",
-//     element: (
-//       <>
-//         <NavBar /> <Projects />
-//       </>
-//     ),
-//   },
-//   {
-//     path: "/contact",
-//     element: (
-//       <>
-//         <NavBar /> <Contact />
-//       </>
-//     ),
-//   },
-// ]);
--- src/Components/NavBarScroll.jsx
+++ src/Components/NavBarScroll.jsx
@@ -3,11 +3,11 @@ import ExternalLinkButton from "./ExternalLinkButton";
 import GitHubIcon from "./Icons/GitHubIcon";
 
 
-export default function NavBarScroll({ scrollToProjects }) {
+export default function NavBarScroll({ scrollToProjects, scrollToHome, scrollToAbout, scrollToContact }) {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 shadow-lg">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <button>
+        <button onClick={scrollToHome}>
           <div className="flex items-center space-x-3 rtl:space-x-reverse">
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               Joe Winter
@@ -17,7 +17,7 @@ export default function NavBarScroll({ scrollToProjects }) {
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <button onClick={scrollToProjects}>
+              <button onClick={scrollToAbout}>
                 <div className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                   About
                 </div>
@@ -31,7 +31,7 @@ export default function NavBarScroll({ scrollToProjects }) {
               </button>
             </li>
             <li>
-              <button onClick={scrollToProjects}>
+              <button onClick={scrollToContact}>
                 <div className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                   Contact
                 </div>
