c40b1a682df90ebc028807b75fc5fee879ebff35
Joe Winter <joewinteruk@gmail.com>
2024-12-10 10:16:18 +0000

    updated about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,26 +10,19 @@ export default function About() {
           a background in structural engineering. In 2023, I graduated from the
           University of Brighton with a master’s degree in civil engineering and
           worked as a structural engineer. However, I never felt satisfied with
-          my work and knew this wasn’t the right path. During my dissertation, I
-          used MATLAB to analyse large amounts of data and found I enjoyed
-          writing and refactoring my code far more than the engineering aspect
-          of the project. This grew into a passion for programming.
+          my work and knew this wasn’t the right path.
         </p>
         <p className="text-lg text-gray-700 dark:text-gray-300 mb-4">
           In June 2024, I decided to make a change. I quit my job and started a
           bootcamp at Makers Academy. Right away, we began learning fundamental
-          concepts like object-oriented design and test-driven development,
-          which I was immediately drawn to. We also practiced pair programming,
-          often completing challenges with members of the bootcamp we hadn’t
-          spoken to before, which helped build collaboration skills.
+          concepts like object-oriented design, test-driven development and pair programming.
         </p>
         <p className="text-lg text-gray-700 dark:text-gray-300 mb-4">
           During the bootcamp, I participated in creating full-stack
           applications as part of a team. In one project, we worked with legacy
           code, understanding, refactoring, and building upon it. In another, we
           started a project entirely from scratch, where I to learned how to use
-          web sockets so we could implement real time functionality, which
-          became a key feature of the application.
+          web sockets so we could implement real time functionality.
         </p>
         <p className="text-lg text-gray-700 dark:text-gray-300">
           As a former structural engineer, I hope to bring my practical
